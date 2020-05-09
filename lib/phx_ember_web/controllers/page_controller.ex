24b1c961ce842547cadb1a0bacc6cd82362ba50a
Mirko Akov <mupkoo@gmail.com>
2020-05-09 15:08:35 +0300

    Inject dummy Phoenix.Token to the Ember index.html file

--- lib/phx_ember_web/controllers/page_controller.ex
+++ lib/phx_ember_web/controllers/page_controller.ex
@@ -2,6 +2,18 @@ defmodule PhxEmberWeb.PageController do
   use PhxEmberWeb, :controller
 
   def index(conn, _params) do
-    html(conn, File.read!("./priv/static/index.html"))
+    content =
+      "./priv/static/index.html"
+      |> File.read!()
+      |> String.replace(
+        "</head>",
+        ~s{<meta name="token" content="#{token(conn)}" /></head>}
+      )
+
+    html(conn, content)
+  end
+
+  defp token(conn) do
+    Phoenix.Token.sign(conn, "user auth", 1)
   end
 end
